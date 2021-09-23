56d3c95a2fcfebebc0473f1306684ba9499fb40e
Roman Gorokhovsky <gorokhovsky@live.com>
2021-09-23 03:36:07 +0300

    Edit test

--- python/api/test_basic.py
+++ python/api/test_basic.py
@@ -1,12 +1,13 @@
 import os
 import unittest
- 
+
 from app import app
- 
-     def test_main_page(self):
-        response = self.app.get('/', follow_redirects=False)
-        self.assertEqual(response.status_code, 200)
- 
- 
+
+
+def test_main_page(self):
+    response = self.app.get('/', follow_redirects=False)
+    self.assertEqual(response.status_code, 200)
+
+
 if __name__ == "__main__":
     unittest.main()
