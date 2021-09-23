09ca5f343ea13743dce3a3a65c38c5843f39fbbd
Roman Gorokhovsky <gorokhovsky@live.com>
2021-09-23 03:46:46 +0300

    Update test_basic.py

--- python/api/test_basic.py
+++ python/api/test_basic.py
@@ -1,12 +1,13 @@
 import os
 import unittest
-import requests
 
 from app import app
 
+
 def test_main_page(self):
-    response = requests.get("http://localhost:5000/?start_date=01.07.2012&end_date=01.07.2013")
-    self.assertEqual(response.status_code, 200)
+    response = self.app.get('/', follow_redirects=False)
+    self.assertEqual(response.status_code, 500)
+
 
 if __name__ == "__main__":
     unittest.main()
