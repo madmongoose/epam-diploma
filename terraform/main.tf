e5d73158fff63de126b66dabb996009cdcdc2a4c
Roman Gorokhovsky <gorokhovsky@live.com>
2021-09-23 05:45:25 +0300

    Add port and change name

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "epm-sg-web" {
   vpc_id      = aws_vpc.epm-vpc-main.id
 
   dynamic "ingress" {
-    for_each = ["22", "80", "443", "8080"]
+    for_each = ["22", "80", "443", "8080", "9000"]
     content {
       from_port   = ingress.value
       to_port     = ingress.value
